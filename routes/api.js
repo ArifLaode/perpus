6ed685646207e642f97a92ef7345193b73ce0c7b
Ariflaode <mumahadariflaode99@gmail.com>
2024-07-26 17:00:11 +0800

    new file:   .gitignore 	new file:   .vscode/settings.json 	new file:   app.js 	new file:   bin/www 	new file:   db/db.js 	new file:   package-lock.json 	new file:   package.json 	new file:   public/bootstrap/dist/css/bootstrap-grid.css 	new file:   public/bootstrap/dist/css/bootstrap-grid.css.map 	new file:   public/bootstrap/dist/css/bootstrap-grid.min.css 	new file:   public/bootstrap/dist/css/bootstrap-grid.min.css.map 	new file:   public/bootstrap/dist/css/bootstrap-grid.rtl.css 	new file:   public/bootstrap/dist/css/bootstrap-grid.rtl.css.map 	new file:   public/bootstrap/dist/css/bootstrap-grid.rtl.min.css 	new file:   public/bootstrap/dist/css/bootstrap-grid.rtl.min.css.map 	new file:   public/bootstrap/dist/css/bootstrap-reboot.css 	new file:   public/bootstrap/dist/css/bootstrap-reboot.css.map 	new file:   public/bootstrap/dist/css/bootstrap-reboot.min.css 	new file:   public/bootstrap/dist/css/bootstrap-reboot.min.css.map 	new file:   public/bootstrap/dist/css/bootstrap-reboot.rtl.css 	new file:   public/bootstrap/dist/css/bootstrap-reboot.rtl.css.map 	new file:   public/bootstrap/dist/css/bootstrap-reboot.rtl.min.css 	new file:   public/bootstrap/dist/css/bootstrap-reboot.rtl.min.css.map 	new file:   public/bootstrap/dist/css/bootstrap-utilities.css 	new file:   public/bootstrap/dist/css/bootstrap-utilities.css.map 	new file:   public/bootstrap/dist/css/bootstrap-utilities.min.css 	new file:   public/bootstrap/dist/css/bootstrap-utilities.min.css.map 	new file:   public/bootstrap/dist/css/bootstrap-utilities.rtl.css 	new file:   public/bootstrap/dist/css/bootstrap-utilities.rtl.css.map 	new file:   public/bootstrap/dist/css/bootstrap-utilities.rtl.min.css 	new file:   public/bootstrap/dist/css/bootstrap-utilities.rtl.min.css.map 	new file:   public/bootstrap/dist/css/bootstrap.css 	new file:   public/bootstrap/dist/css/bootstrap.css.map 	new file:   public/bootstrap/dist/css/bootstrap.min.css 	new file:   public/bootstrap/dist/css/bootstrap.min.css.map 	new file:   public/bootstrap/dist/css/bootstrap.rtl.css 	new file:   public/bootstrap/dist/css/bootstrap.rtl.css.map 	new file:   public/bootstrap/dist/css/bootstrap.rtl.min.css 	new file:   public/bootstrap/dist/css/bootstrap.rtl.min.css.map 	new file:   public/bootstrap/dist/js/bootstrap.bundle.js 	new file:   public/bootstrap/dist/js/bootstrap.bundle.js.map 	new file:   public/bootstrap/dist/js/bootstrap.bundle.min.js 	new file:   public/bootstrap/dist/js/bootstrap.bundle.min.js.map 	new file:   public/bootstrap/dist/js/bootstrap.esm.js 	new file:   public/bootstrap/dist/js/bootstrap.esm.js.map 	new file:   public/bootstrap/dist/js/bootstrap.esm.min.js 	new file:   public/bootstrap/dist/js/bootstrap.esm.min.js.map 	new file:   public/bootstrap/dist/js/bootstrap.js 	new file:   public/bootstrap/dist/js/bootstrap.js.map 	new file:   public/bootstrap/dist/js/bootstrap.min.js 	new file:   public/bootstrap/dist/js/bootstrap.min.js.map 	new file:   public/css/index.css 	new file:   public/daftarAnggota.html 	new file:   public/daftarBuku.html 	new file:   public/daftarPeminjam.html 	new file:   public/index.html 	new file:   routes/api.js 	new file:   routes/index.js 	new file:   routes/users.js

---
+++ routes/api.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db/db');
+
+function getData(url, tb_name) {
+    var sql = `SELECT * FROM ${tb_name}`;
+    router.get(url, (req, res) => {
+        db.query(sql, (err, data) => {
+            if (err) {
+                console.log(`Tidak bisa mengambil data di tabel ${tb_name} dengan error: ${err}`);
+                res.status(500).send('Gagal mengambil data.');
+            } else {
+                res.send(data);
+            }
+        });
+    });
+}
+
+function editData(url, tb_name) {
+    router.post(url, (req, res) => {
+        const id = req.params.id;
+        const { judulBuku, pengarang } = req.body;
+        
+        const sql = `UPDATE ${tb_name} SET judulBuku = ?, pengarang = ? WHERE id = ?`;
+        const values = [judulBuku, pengarang, id];
+
+        db.query(sql, values, (err, result) => {
+            if (err) {
+                console.log(`Tidak bisa mengupdate data di tabel ${tb_name} dengan error: ${err}`);
+                res.status(500).json({ error: 'Gagal mengupdate data.' });
+            } else {
+                console.log(`Berhasil mengupdate data di tabel ${tb_name} dengan id: ${id}`);
+                res.json({ message: 'Data berhasil diupdate', id: id });
+            }
+        });
+    });
+}
+
+function addData(url, tb_name) {
+    var data = req.body;
+    console.log(data);
+    var sql = `INSERT INTO ${tb_name} (${data}) VALUES (${data})`;
+    db.query(sql, (err, data) => {
+        if (err) {
+            console.log(`Tidak bisa menambah data di tabel ${tb_name} dengan error: ${err}`);
+            res.status(500).send('Gagal menambah data.');
+        } else {
+            console.log(`Berhasil menambah data di tabel ${tb_name} dengan data: ${data}`);
+            res.send(data);
+        }
+    });
+}
+
+
+getData('/dataPeminjam', 'peminjam');
+addData('dataPeminjam', 'peminjam');
+addData('/addBuku', 'buku');
+getData('/dataBuku', 'buku');
+editData('/editBuku/:id', 'buku');
+
+module.exports = router;
